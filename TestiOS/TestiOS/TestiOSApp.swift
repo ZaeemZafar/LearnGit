2868537435b7c1a599db1f481bd7e71a5dcb898c
zaeemzafar <zaeem@citrusbits.com>
2023-02-20 18:48:48 +0500

    Add initial project files
    Started a new project using xcode

---
+++ TestiOS/TestiOS/TestiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestiOSApp.swift
+//  TestiOS
+//
+//  Created by zaeemzafar on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
