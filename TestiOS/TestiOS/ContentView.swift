4df24009020777e4a0d6fd1823c92108b2dfcdb3
zaeem <zaeem@citrusbits.com>
2023-04-26 15:59:59 +0500

    Squashed commit of the following:
    commit 8c1200ea72ab31bf02c7f8a3489b9ed286cd68dd
    Author: zaeem <zaeem@citrusbits.com>
    Date:   Wed Apr 26 15:50:03 2023 +0500
    
        Update for PR # 2
    
    commit 71b3e339a95d66ef2bf45c48bbc396ce3b371c78
    Author: zaeem <zaeem@citrusbits.com>
    Date:   Wed Apr 26 15:46:36 2023 +0500
    
        Update ContentView.swift
    
    commit 2dcc2c4696893234e644424fe25daac0b411059a
    Author: zaeem <zaeem@citrusbits.com>
    Date:   Mon Feb 27 14:05:25 2023 +0500
    
        Update ContentView.swift

--- TestiOS/TestiOS/ContentView.swift
+++ TestiOS/TestiOS/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globel")
+            Image(systemName: "globel namespace Testing PR")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world! Nice to see yeah! Oh wait.")
+            Text("Hello, world! Nice to see yeah! Oh wait. PR #2")
         }
         .padding()
     }
